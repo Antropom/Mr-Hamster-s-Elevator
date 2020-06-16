7a441151c98cf3703fcd4a9bb2cbed007c39d8dd
Louis Collet <colletlouis@gmail.com>
2020-06-16 19:45:02 +0200

    Buttons can now move elevator

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Creation of the Elevator class
 class Elevator {
-  constructor() {
-    this._minFloor = -1;
-    this._maxFloor = 7;
+  constructor(minFloor, maxFloor) {
+    this._minFloor = minFloor;
+    this._maxFloor = maxFloor;
     this._currentFloor = 0;
   }
 
@@ -39,13 +39,31 @@ class Elevator {
 }
 
 // Creation of Mr Hamster's Elevator
-const HamsterElev = new Elevator();
+const hamsterElev = new Elevator(-1, 7);
 
 // Creation of the keyboard
 const myButtons = document.getElementById('my-buttons');
 const floors = [];
-for (let i = HamsterElev.minFloor; i <= HamsterElev.maxFloor; i++) {
+for (let i = hamsterElev.minFloor; i <= hamsterElev.maxFloor; i++) {
   floors.push(i);
 }
-const buttonsHTML = floors.map((floor) => `<button>${floor}</button>`).join('');
+const buttonsHTML = floors
+  .map((floor) => `<button class="btn">${floor}</button>`)
+  .join('');
 myButtons.innerHTML = buttonsHTML;
+
+// Current floor in HTML title
+const modifyFloorTitle = (floor) => {
+  const title = document.getElementById('title');
+  title.innerHTML = `You're currently on the floor : ${floor}`;
+};
+
+document.body.onload = modifyFloorTitle(hamsterElev.currentFloor);
+
+// Change current floor when clicking on the buttons
+document.querySelectorAll('.btn').forEach((button) =>
+  button.addEventListener('click', () => {
+    hamsterElev.toFloor(button.innerHTML);
+    modifyFloorTitle(hamsterElev.currentFloor);
+  })
+);
